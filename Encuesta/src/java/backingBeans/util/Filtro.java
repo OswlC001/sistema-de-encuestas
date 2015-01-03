af12efdb683307201f3de3de3495d330bf864337
0swllll9 <0swllll9@gmail.com>
2015-01-03 15:48:56 +0000

    Que solo el administrador tenga acceso a administrar

--- Encuesta/src/java/backingBeans/util/Filtro.java
+++ Encuesta/src/java/backingBeans/util/Filtro.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author SiperProg
+ * @author Oswaldo Cruz
  */
 @WebFilter(filterName = "Filtro", urlPatterns = {"/*"})
 public class Filtro implements Filter {
